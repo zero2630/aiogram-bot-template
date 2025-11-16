9d5587b84260637f7f898c6acacb8987e5415b01
zero <ivanmanannikov96@gmail.com>
2025-11-16 20:36:26 +0300

    Add args in database.py; Update migrations

--- app/db/database.py
+++ app/db/database.py
@@ -1,4 +1,5 @@
 import asyncio  # noqa: F401
+import sys
 
 from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
@@ -18,11 +19,36 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def create_tables():
-    from models import Base
+    from app.db.models.base import Base
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def drop_tables():
+    from app.db.models.base import Base
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+async def recreate_tables():
+    from app.db.models.base import Base
 
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+async def main():
+    args = sys.argv
+    if len(args) == 2:
+        param = args[1]
+        if param == "create":
+            create_tables()
+        elif param == "drop":
+            drop_tables()
+        elif param == "recreate":
+            recreate_tables()
+
 
 
 if __name__ == "__main__":
--- migrations/env.py
+++ migrations/env.py
@@ -5,19 +5,20 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from other.conf import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS
-from other.models import Base
+from app.config.settings import get_settings
+from app.db.models.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+settings = get_settings()
 
 section = config.config_ini_section
-config.set_section_option(section, "DB_HOST", DB_HOST)
-config.set_section_option(section, "DB_PORT", DB_PORT)
-config.set_section_option(section, "DB_NAME", DB_NAME)
-config.set_section_option(section, "DB_USER", DB_USER)
-config.set_section_option(section, "DB_PASS", DB_PASS)
+config.set_section_option(section, "DB_HOST", settings.DB_HOST)
+config.set_section_option(section, "DB_PORT", settings.DB_PORT)
+config.set_section_option(section, "DB_NAME", settings.DB_NAME)
+config.set_section_option(section, "DB_USER", settings.DB_USER)
+config.set_section_option(section, "DB_PASS", settings.DB_PASS)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
